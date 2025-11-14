c8afe9e8dc2752ac1c5bab83d2bab78b8b08c627
ZvRzyan18 <rhenzalfred06@gmail.com>
2025-11-14 23:58:31 +0800

    Create area2d.h

---
+++ src/neophysics/p2d/area2d.h
@@ -0,0 +1,13 @@
+#ifndef NP_AREA2D_H
+#define NP_AREA2D_H
+
+#include "neophysics/math.h"
+#include "neophysics/core2d.h"
+
+NP_COLD
+NP_INLINE NPReal __NPPolygon2DArea(NPReal2 *vertices, NPUint8 len, NPReal2 scale) {
+ return 0;
+}
+
+#endif
+
